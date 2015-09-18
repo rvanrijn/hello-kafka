bb529366bb5d4c6ac61c865e6c926045b5ea68d4
Robert van Rijn <r.van.rijn@ncim.nl>
2015-09-18 13:32:16 +0200

    adjusted tests with the right ip’s and port numbers

--- src/test/scala/KafkaConfigSpec.scala
+++ src/test/scala/KafkaConfigSpec.scala
@@ -5,12 +5,12 @@ class KafkaConfigSpec extends FunSuite {
   val config = new KafkaConfig {}
 
   test("Consumer config should be read") {
-    assert(config.getProperty("group.id") == "1234")
-    assert(config.getProperty("zookeeper.connect") == "localhost:2821")
+    assert(config.getProperty("group.id") == "test-consumer-group")
+    assert(config.getProperty("zookeeper.connect") == "localhost:2181")
   }
 
   test("example.producer.Producer config should be read") {
-    assert(config.getProperty("metadata.broker.list") == "broker1:9092,broker2:9092")
+    assert(config.getProperty("metadata.broker.list") == "localhost:9092")
     assert(config.getProperty("serializer.class") == "kafka.serializer.StringEncoder")
     assert(config.getProperty("partitioner.class") == "example.producer.SimplePartitioner")
     assert(config.getProperty("request.required.acks") == "1")

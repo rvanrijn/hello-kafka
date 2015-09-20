4fc73d785a7fbd6deaf8e3c3ac87bffc54ce875b
Robert van Rijn <r.van.rijn@ncim.nl>
2015-09-20 15:34:25 +0200

    changed topic name

--- src/main/scala/example/producer/ProducerStreamExample.scala
+++ src/main/scala/example/producer/ProducerStreamExample.scala
@@ -4,7 +4,7 @@ import scala.util.Random
 
 object ProducerStreamExample {
   def main(args: Array[String]): Unit = {
-    val strProducer = Producer[String]("TutorialTopic")
+    val strProducer = Producer[String]("test")
 
     val messageStream = Stream.continually{
       Random.alphanumeric.take(5).mkString
